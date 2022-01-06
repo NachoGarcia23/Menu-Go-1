6c65b28578566cf201e828260528c818bd829659
Maximiliano Saldivia <saldiviamaximiliano2@gmail.com>
2022-01-06 01:46:06 -0300

    Primeros cambios

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import Carousel from './Carousel';
+import Cards from './Cards';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Carousel />
+      <Cards/>
+    </div>
+  );
+}
+
+export default App;
